d833b22d11687236a4e2316527fa9099b23111fd
thursandi <thursandi@gmail.com>
2017-11-29 09:31:19 +0700

    Delete test.php

--- absensi/controllers/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$awal = date('Y-m-01 00:00:00');
-	$akhir = date('Y-m-31 00:00:00');
-
-	echo $awal." ".$akhir."<br>";
-	echo date('Y-m-d',strtotime('+ 3 day'));
-
-	echo "<br>".strtotime(date('Y-m-d H:i:s'));
-?>
